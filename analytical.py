7dd4dc00130443cd18f7f1bebf4da0a37eaed1a9
gdanezis <g.danezis@ucl.ac.uk>
2020-03-24 20:10:17 +0000

    Added notebook

--- analytical.py
+++ analytical.py
@@ -39,7 +39,7 @@ def estimate(R, Dp, pop, CFR_covid=0.001, hospital_infection_mult = 1.0):
     converge = False
     for i in range(1_000_000):
 
-        # Equations defining the model
+        # Equations defining the modelS
 
         # In hostpital infection is as if you had 'hospital_infection_mult'
         # independent chances of catching according to prevalence
@@ -50,17 +50,19 @@ def estimate(R, Dp, pop, CFR_covid=0.001, hospital_infection_mult = 1.0):
         actual_DP = max(Dp - hospital_prev * CFR_other * pop, 1)
 
         # The prevalence is the number of resolved cases / population
-        prev = (Rp + actual_DP) / pop
+        prev = (Rp + Dp) / pop
 
         # The testing rate of the non-dead population.
         # (We make an assumption that dead people are nearly all diagnosed
         # when positive with the virus).
         f = R / Rp
 
-        # Update Rp to convergence
+        
         # Based on CFR = D / R+D <=> R = D/cfr - D
-        Rp2 = actual_DP / CFR_covid - actual_DP
-        DRp = Rp - Rp2                
+        Rp_new = actual_DP / CFR_covid - Dp
+
+        # Update Rp to convergence
+        DRp = Rp - Rp_new           
         new_Rp = Rp + alpha * DRp
         Rp = bound(new_Rp, 1.0, pop - Dp)
 
@@ -94,12 +96,8 @@ populations = {
         'France' : (2200, 860, 66_990_000)
 }
 
-# Print a table with all countries
 
-CFR_covid = 0.001
-hospital_infection_mult = 10
-
-with open(r'figures/table.txt', 'w') as flx:
+def make_table(populations, CFR_covid, hospital_infection_mult, flx):
     print(f'Assumptions: COVID CFR: {100*CFR_covid:>5.2f}% In Hospital factor: x{hospital_infection_mult}', file=flx)
     print(f"{'Country':15}   Prev      CFR  Testing   Comorb.    Infected", file=flx)
     print('-'* 62)
@@ -131,17 +129,11 @@ with open(r'figures/table.txt', 'w') as flx:
         assert (small(cfr - (Dp / (Rp + Dp)), 0.01))
         assert (small(R -  f * Rp, 0.01))
     
-    
-
-
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(111)
-
-def estimate_project(R, D, CFR_covid, hospital_infection_mult):
+def estimate_project(R, D, CFR_covidx, hospital_infection_mult):
     prev_y = []
     for ri, di in zip(R, D):
-        converge, i, vals = estimate(ri, di, pop, CFR_covid, hospital_infection_mult)
-        prev, CFR_covid, Rp, f, CFR_other = vals
+        converge, i, vals = estimate(ri, di, pop, CFR_covidx, hospital_infection_mult)
+        prev, CFR_covidx, Rp, f, CFR_other = vals
         prev_y += [ prev ]
 
     ## Plot a projection!
@@ -166,109 +158,76 @@ def estimate_project(R, D, CFR_covid, hospital_infection_mult):
 
     return (Rindex, prev_y, proj_dates, proj_prev)
 
+if __name__ == '__main__':
+    # Print a table with all countries
+    CFR_covid = 0.001
+    hospital_infection_mult = 10
 
+    with open(r'figures/table.txt', 'w') as flx:
+        make_table(populations, CFR_covid, hospital_infection_mult, flx)
 
-D = pd.read_csv(r'data\time_series_19-covid-Deaths.csv')
-R = pd.read_csv(r'data\time_series_19-covid-Recovered.csv')
-C = pd.read_csv(r'data\time_series_19-covid-Confirmed.csv')
-
-dataset = []
-
-c = 0
-for (_, Rrow), (_, Drow), (_, Crow) in zip(R.iterrows(), D.iterrows(), C.iterrows()):
-    country = Rrow[1]
-    S = Rrow[4:] + Drow[4:]
-    
-
-    if S[-1] > 10 and country in populations:
-        pop = populations[country][-1]
-        mask = (S > 50)
-        
-        R = Rrow[4:][mask]
-        D = Drow[4:][mask]
-
-        if not len(D) > 3:
-            continue
-        
-        start = 0.001
-
-        vals = estimate_project(R, D, CFR_covid, hospital_infection_mult)
-        (Rindex, prev_y, proj_dates, proj_prev) = vals
-        
-        dates_x = list(map(convert_date, R.index))
-        plt.plot_date(dates_x, prev_y, fmt='*-', label = country, color=f'C{c}')
-
-        dates_proj = list(map(date2num, proj_dates))
-        plt.plot_date(dates_proj, proj_prev, fmt='--', label = None, color=f'C{c}')
- 
-
-        vals_lo = estimate_project(R, D, CFR_covid/10.0, hospital_infection_mult)
-        (_, prev_y_lo, _, proj_prev_lo) = vals_lo
-
-        vals_hi = estimate_project(R, D, CFR_covid * 10, hospital_infection_mult)
-        (_, prev_y_hi, _, proj_prev_hi) = vals_hi
-
-        assert len(dates_x) == len(prev_y)
-        assert len(dates_x) == len(prev_y_lo)
-        assert len(dates_x) == len(prev_y_hi)
-        
-        dataset += [( dates_x, prev_y, prev_y_lo, prev_y_hi,
-                      dates_proj, proj_prev, proj_prev_lo, proj_prev_hi,
-                      f'C{c}', country )]
-
-        # Update the color counter
-        c += 1
-
-
-plt.axhline(y=0.65, color='r', linestyle='-', alpha=0.3)
-plt.axvline(x=date2num(datetime.today()), color='k', linestyle='-', alpha=0.3)
-
-
-plt.yscale('log')
-plt.legend(loc=2)
-
-
-rule = rrulewrapper(WEEKLY, byweekday=MO)
-loc = RRuleLocator(rule)
-formatter = DateFormatter('%y-%m-%d')
-
-ax.xaxis.set_major_locator(loc)
-ax.xaxis.set_major_formatter(formatter)
-ax.xaxis.set_tick_params(rotation=30, labelsize=10)
-
-ax.yaxis.set_major_formatter(
-    matplotlib.ticker.FuncFormatter(lambda x, p: f'{100*x:4.1f}%'))
-
-
-
-plt.ylabel('Prevalence, Estimated and Projected')
-plt.xlabel('Date')
-plt.title(f'COVID-19 Prevalence: Estimation & Projection (CFR Line 0.1%)')
-
-plt.grid()
-plt.savefig(f'figures/All-prev.png', dpi=300)
-plt.close()
-
-# Make all country graphs
-
-for rec in dataset:
-    ( dates_x, prev_y, prev_y_lo, prev_y_hi,
-                      dates_proj, proj_prev, proj_prev_lo, proj_prev_hi,
-                      col, country ) = rec
-
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(14,7))
     ax = fig.add_subplot(111)
-    
-    plt.plot_date(dates_x, prev_y, fmt='*-', label = country, color=col)
-    plt.plot_date(dates_proj, proj_prev, fmt='--', label = None, color=col)
-    plt.fill_between(x=dates_x, y1=prev_y_lo, y2=prev_y_hi, alpha=0.2, color=col)
-    plt.fill_between(x=dates_proj, y1=proj_prev_lo, y2=proj_prev_hi, alpha=0.2, color=col)
+
+    D = pd.read_csv(r'data\time_series_19-covid-Deaths.csv')
+    R = pd.read_csv(r'data\time_series_19-covid-Recovered.csv')
+    C = pd.read_csv(r'data\time_series_19-covid-Confirmed.csv')
+
+    dataset = []
+
+    c = 0
+    for (_, Rrow), (_, Drow), (_, Crow) in zip(R.iterrows(), D.iterrows(), C.iterrows()):
+        country = Rrow[1]
+        S = Rrow[4:] + Drow[4:]
+        
+
+        if S[-1] > 10 and country in populations:
+            pop = populations[country][-1]
+            mask = (S > 50)
+            
+            R = Rrow[4:][mask]
+            D = Drow[4:][mask]
+
+            if not len(D) > 3:
+                continue
+            
+            start = 0.001
+
+            vals = estimate_project(R, D, CFR_covid, hospital_infection_mult)
+            (Rindex, prev_y, proj_dates, proj_prev) = vals
+            
+            dates_x = list(map(convert_date, R.index))
+            plt.plot_date(dates_x, prev_y, fmt='*-', label = country, color=f'C{c}')
+
+            dates_proj = list(map(date2num, proj_dates))
+            plt.plot_date(dates_proj, proj_prev, fmt='--', label = None, color=f'C{c}')
+     
+
+            vals_lo = estimate_project(R, D, CFR_covid/10.0, hospital_infection_mult)
+            (_, prev_y_lo, _, proj_prev_lo) = vals_lo
+
+            vals_hi = estimate_project(R, D, CFR_covid * 10, hospital_infection_mult)
+            (_, prev_y_hi, _, proj_prev_hi) = vals_hi
+
+            assert len(dates_x) == len(prev_y)
+            assert len(dates_x) == len(prev_y_lo)
+            assert len(dates_x) == len(prev_y_hi)
+            
+            dataset += [( dates_x, prev_y, prev_y_lo, prev_y_hi,
+                          dates_proj, proj_prev, proj_prev_lo, proj_prev_hi,
+                          f'C{c}', country )]
+
+            # Update the color counter
+            c += 1
+
 
     plt.axhline(y=0.65, color='r', linestyle='-', alpha=0.3)
     plt.axvline(x=date2num(datetime.today()), color='k', linestyle='-', alpha=0.3)
 
+
     plt.yscale('log')
     plt.legend(loc=2)
+
 
     rule = rrulewrapper(WEEKLY, byweekday=MO)
     loc = RRuleLocator(rule)
@@ -282,13 +241,55 @@ for rec in dataset:
         matplotlib.ticker.FuncFormatter(lambda x, p: f'{100*x:4.1f}%'))
 
 
+
     plt.ylabel('Prevalence, Estimated and Projected')
     plt.xlabel('Date')
-    plt.title(f'{country} COVID-19 Prevalence: Estimation & Projection (CFR Line 0.1% [0.01%-1%])')
+    plt.title(f'COVID-19 Prevalence: Estimation & Projection (CFR Line 0.1%)')
 
     plt.grid()
-    plt.savefig(f'figures/{country}-prev.png', dpi=300)
+    plt.savefig(f'figures/All-prev.png', dpi=300)
     plt.close()
 
+    # Make all country graphs
 
-    
+    for rec in dataset:
+        ( dates_x, prev_y, prev_y_lo, prev_y_hi,
+                          dates_proj, proj_prev, proj_prev_lo, proj_prev_hi,
+                          col, country ) = rec
+
+        fig = plt.figure(figsize=(14,7))
+        ax = fig.add_subplot(111)
+        
+        plt.plot_date(dates_x, prev_y, fmt='*-', label = country, color=col)
+        plt.plot_date(dates_proj, proj_prev, fmt='--', label = None, color=col)
+        plt.fill_between(x=dates_x, y1=prev_y_lo, y2=prev_y_hi, alpha=0.2, color=col)
+        plt.fill_between(x=dates_proj, y1=proj_prev_lo, y2=proj_prev_hi, alpha=0.2, color=col)
+
+        plt.axhline(y=0.65, color='r', linestyle='-', alpha=0.3)
+        plt.axvline(x=date2num(datetime.today()), color='k', linestyle='-', alpha=0.3)
+
+        plt.yscale('log')
+        plt.legend(loc=2)
+
+        rule = rrulewrapper(WEEKLY, byweekday=MO)
+        loc = RRuleLocator(rule)
+        formatter = DateFormatter('%y-%m-%d')
+
+        ax.xaxis.set_major_locator(loc)
+        ax.xaxis.set_major_formatter(formatter)
+        ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+
+        ax.yaxis.set_major_formatter(
+            matplotlib.ticker.FuncFormatter(lambda x, p: f'{100*x:4.1f}%'))
+
+
+        plt.ylabel('Prevalence, Estimated and Projected')
+        plt.xlabel('Date')
+        plt.title(f'{country} COVID-19 Prevalence: Estimation & Projection (CFR Line 0.1% [0.01%-1%])')
+
+        plt.grid()
+        plt.savefig(f'figures/{country}-prev.png', dpi=300)
+        plt.close()
+
+
+        
